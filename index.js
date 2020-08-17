c920c53c09582339bfa89cae2aeb44802aac7d2c
Asma Kolaib <as.kolaib@gmail.com>
2020-08-17 05:10:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ class Circle {
   set diameter( diameter ) {
     this.radius = diameter / 2
   }
+    get area() {
+    return pi * ( this.radius * this.radius )
+  }
   get circumference() {
     return ( this.radius * 2 ) * pi
   }
   set circumference( circumference ) {
     this.radius = circumference / ( pi * 2 )
   }
-  get area() {
-    return pi * ( this.radius * this.radius )
-  }
+
   set area( area ) {
     this.radius = Math.sqrt( area / pi )
   }
